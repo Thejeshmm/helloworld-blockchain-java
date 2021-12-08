8204e923867362366ceacb015b3ef52ca1d52f49
邢开春 <409060350@qq.com>
2021-12-08 13:51:30 +0800

    20211208v03

--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/tool/ContractGeneratorTool.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/tool/ContractGeneratorTool.java
@@ -8,7 +8,7 @@ public class ContractGeneratorTool {
 
     public static void main(String[] args) {
 
-        String contractPath = "";
+        String contractPath = "E:\\workspace\\JavaProjects\\helloworld-language\\src\\test\\resources\\nft.helloworld";
         System.out.println(ByteUtil.bytesToHexString(ByteUtil.stringToUtf8Bytes(FileUtil.read(contractPath))));
     }
 }
