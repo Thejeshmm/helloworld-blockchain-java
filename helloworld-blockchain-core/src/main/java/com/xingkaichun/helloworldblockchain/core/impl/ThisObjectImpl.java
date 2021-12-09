d160a94806ba4e12cfe9dc08ea20302d7b1748fd
邢开春 <409060350@qq.com>
2021-12-09 14:38:56 +0800

    20211209v04

--- helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/impl/ThisObjectImpl.java
+++ helloworld-blockchain-core/src/main/java/com/xingkaichun/helloworldblockchain/core/impl/ThisObjectImpl.java
@@ -1,25 +1,33 @@
 package com.xingkaichun.helloworldblockchain.core.impl;
 
 import com.xingkaichun.helloworldblockchain.core.model.transaction.Transaction;
-import com.xingkaichun.helloworldblockchain.core.model.transaction.TransactionOutput;
+import com.xingkaichun.helloworldblockchain.crypto.AccountUtil;
 import com.xingkaichun.helloworldblockchain.language.virtualmachine.ThisObject;
 import com.xingkaichun.helloworldblockchain.language.virtualmachine.VirtualMachine;
 
 public class ThisObjectImpl extends ThisObject {
 
-    private TransactionOutput transactionOutput;
+    private Transaction transaction;
 
     public ThisObjectImpl(VirtualMachine virtualMachine, Transaction transaction) {
         this.virtualMachine = virtualMachine;
-        this.transactionOutput = transactionOutput;
+        this.transaction = transaction;
     }
 
     public int newThisAddress(){
         int thisAddress = virtualMachine.newObject(1);
         int transactionAddress = virtualMachine.newObject(2);
         virtualMachine.storeObjectField(thisAddress,0,transactionAddress);
-        int intFromAddress = virtualMachine.newString(transactionOutput.getAddress());
-        int intToAddress = virtualMachine.newString(transactionOutput.getAddress());
+        String address = null;
+        //TODO
+        if(transaction.getInputs() == null || transaction.getInputs().isEmpty()){
+            address = AccountUtil.addressFromPublicKeyHash("00");
+        }else {
+            String publicKeyHash = transaction.getInputs().get(0).getInputScript().get(3);
+            address = AccountUtil.addressFromPublicKeyHash(publicKeyHash);
+        }
+        int intFromAddress = virtualMachine.newString(address);
+        int intToAddress = virtualMachine.newString(transaction.getOutputs().get(0).getAddress());
         virtualMachine.storeObjectField(transactionAddress,0,intFromAddress);
         virtualMachine.storeObjectField(transactionAddress,1,intToAddress);
         return thisAddress;
